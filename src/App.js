af87e391b947bd91882a7791f2518b4514083f74
durgaraotarra <durgarao.tarra@gmail.com>
2023-04-02 15:27:23 +0530

    Add contact form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Layout from './pages/Layout';
 import Products from './pages/Products';
@@ -11,6 +12,7 @@ function App() {
         <Route element={<Layout />}>
           <Route path='/' exact element={<Home />} />
           <Route path='/products' element={<Products />} />
+          <Route path='/contact' element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,21 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 const Contact = () => {
-    
+    return(
+        <Box
+            component="form" noValidate autoComplete="off"
+        >
+            <div>
+                <TextField
+                    error
+                    id="outlined-error"
+                    label="error"
+                    defaultValue="Hello World"
+                    />
+            </div>
+        </Box>
+    )
 }
+
+export default Contact
